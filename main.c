a51a52b08012bb0ef71f1c11e943113c01fa0db3
wnstjs1013 <joonsunyeu@gmail.com>
2024-09-06 16:01:40 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/* 배열 실습1(난수생성) */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number[10];
+
+    srand(time(NULL));
+
+    for(int i = 0; i < 10; i++)
+        {
+            number[i] = (rand() % 100) +1;
+        }
+
+
+    for(int i = 0; i < 10; i++)
+        {
+            printf("number = %d\n", number[i]);
+        }
+
+    return 0;
+}
